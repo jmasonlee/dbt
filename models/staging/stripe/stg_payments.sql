a3b27d1f118dbbd1354e78dd53cdc4e8a1ac9ea3
jbilston@yelp.com <jbilston@yelp.com>
2022-01-31 13:48:09 +0000

    B## Update payments sql

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,5 +1,12 @@
 with payments as (
-    select orderid, amount / 100 as amount
+    select
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status,
+
+    amount / 100 as amount,
+    created as created_at
     from `dbt-tutorial.stripe.payment`
 )
 
