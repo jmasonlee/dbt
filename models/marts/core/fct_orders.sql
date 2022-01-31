396a2b9cad9616d7cb0ca4ce6c320f62684550ad
jbilston@yelp.com <jbilston@yelp.com>
2022-01-31 13:53:31 +0000

    B## Fix things to work with exercise

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -6,11 +6,25 @@ payments as (
     select * from {{ref("stg_payments")}}
 ),
 
-fct_orders as (
-    select o.order_id, o.customer_id, p.amount
-    from orders o
-    join  
-    payments p on o.order_id = p.orderid
+order_payments as (
+    select
+        order_id,
+        sum(case when status = 'success' then amount end) as amount
+
+    from payments
+    group by 1
+),
+
+final as (
+
+    select
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date,
+        coalesce(order_payments.amount, 0) as amount
+
+    from orders
+    left join order_payments using (order_id)
 )
 
-select * from fct_orders
+select * from final
